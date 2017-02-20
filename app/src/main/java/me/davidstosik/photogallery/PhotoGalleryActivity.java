8f702b208b829c239cd09310a2e07b265763d5ef
David Stosik <david.stosik+git-noreply@gmail.com>
2017-02-21 08:24:48 +0900

    Chap 26 - Set up a background service to poll Flickr for new pictures

--- app/src/main/java/me/davidstosik/photogallery/PhotoGalleryActivity.java
+++ app/src/main/java/me/davidstosik/photogallery/PhotoGalleryActivity.java
@@ -1,9 +1,14 @@
 package me.davidstosik.photogallery;
 
-import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, PhotoGalleryActivity.class);
+    }
 
     @Override
     public Fragment createFragment() {
--- app/src/main/java/me/davidstosik/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/me/davidstosik/photogallery/PhotoGalleryFragment.java
@@ -1,5 +1,6 @@
 package me.davidstosik.photogallery;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -182,6 +183,13 @@ public class PhotoGalleryFragment extends Fragment {
                 return false;
             }
         });
+
+        MenuItem toggleItem = menu.findItem(R.id.menu_item_toggle_polling);
+        if (PollService.isServiceAlarmOn(getActivity())) {
+            toggleItem.setTitle(R.string.stop_polling);
+        } else {
+            toggleItem.setTitle(R.string.start_polling);
+        }
     }
 
     @Override
@@ -192,12 +200,18 @@ public class PhotoGalleryFragment extends Fragment {
                 clearItems();
                 updateItems();
                 return true;
+            case R.id.menu_item_toggle_polling:
+                boolean shouldStartAlarm = !PollService.isServiceAlarmOn(getActivity());
+                PollService.setServiceAlarm(getActivity(), shouldStartAlarm);
+                getActivity().invalidateOptionsMenu();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
     private void clearItems() {
+        Log.d(TAG, "clearItems: ");
         mLastPage = 0;
         int oldSize = mItems.size();
         mItems.clear();
@@ -224,6 +238,7 @@ public class PhotoGalleryFragment extends Fragment {
     }
 
     private void updateAdapter(int positionStart, int size) {
+        Log.d(TAG, "updateAdapter: ");
         PhotoAdapter adapter = (PhotoAdapter) mPhotoRecyclerView.getAdapter();
         adapter.notifyItemRangeInserted(positionStart, size);
         if (mLastPage > 0) {
@@ -331,6 +346,7 @@ public class PhotoGalleryFragment extends Fragment {
         }
 
         private void preloadInterval(int first, int last) {
+            Log.d(TAG, "preloadInterval: first=" + String.valueOf(first) + ", last=" + String.valueOf(last));
             for (int i = first; i <= last; i++) {
                 try {
                     String url = mGalleryItems.get(i).getUrl();
